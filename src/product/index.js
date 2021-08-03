e62cb636699982bb132e9960ca1b451480328214
CNSITM <psr@cnsitm.com>
2021-08-03 10:40:37 +0900

    antd 기본 적용

--- src/product/index.js
+++ src/product/index.js
@@ -1,8 +1,44 @@
 import { useParams } from "react-router-dom";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import "./index.css";
 
 function ProductPage() {
   const { id } = useParams();
-  return <h1> 상품 상세 페이지 {id} 상품</h1>;
+  const [product, setProsuct] = useState(null);
+  useEffect(function () {
+    axios
+      .get(
+        `https://73a026d6-fdc4-412d-ba6f-6fe851dda36d.mock.pstmn.io/products/${id}`
+      )
+      .then(function (result) {
+        setProsuct(result.data);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }, []);
+  if (product === null) {
+    return <h1>상품 정보를 받고 있습니다...</h1>;
+  }
+
+  return (
+    <div>
+      <div id="image-box">
+        <img src={"/" + product.imageUrl} />
+      </div>
+      <div id="profile-box">
+        <img src="/images/icons/avatar.png" />
+        <span>{product.seller}</span>
+      </div>
+      <div id="contents-box">
+        <div id="name">{product.name}</div>
+        <div id="price">{product.price}원</div>
+        <div id="createdAt">2021년 08월 02일</div>
+        <div id="description">{product.description}</div>
+      </div>
+    </div>
+  );
 }
 
 export default ProductPage;
